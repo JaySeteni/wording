96b3cb13cb405da70f57e07d8c1a2a4c3cb6a558
JaySeteni <163840333+JaySeteni@users.noreply.github.com>
2026-02-09 22:45:27 +0000

    default routing setup saved

--- wording/src/app/app.routes.ts
+++ wording/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './admin/login/login';
+import { Signup } from './admin/signup/signup';
 
-export const routes: Routes = [];
+
+
+
+
+export const routes: Routes = [
+
+  { path: '', component: Login },      // default route
+  { path: 'login', component: Login }, // /login
+  { path: 'signup', component: Signup }, // /signup
+  { path: '**', redirectTo: '' }                // wildcard
+];;
